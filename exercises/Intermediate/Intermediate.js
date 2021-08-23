a83661bbbab70ac19528d96ff198979240eaa9c6
NGrus <82804457+NGrus@users.noreply.github.com>
2021-08-23 16:48:10 +0200

    all until P

--- exercises/Intermediate/Intermediate.js
+++ exercises/Intermediate/Intermediate.js
@@ -5,15 +5,28 @@ function ExL(){
   let x = 50;
   document.getElementById("ExL").innerHTML=x
 }
+
+
 function ExM(){
-  const car ={
-    company="Fiat", 
-    model="500",
-    state={
-      bad="rusted to hell",
-      medium="Light rust",
-      good="no rust",
-    }
+  const car={
+    company:"Fiat", 
+    model:"500",
+      state:{
+        bad:"rusted to hell",
+        medium:"Light rust",
+        good:"no rust",
+      },
+  };
+  document.getElementById("ExM").innerHTML="Car is a "+car.company +" model "+ car.model +" and "+car.state.bad
+}
+ExO = () => {
+  document.getElementById("ExO").innerHTML="It Worked"
+}
+ExP = () => {
+  try {
+    nonfunct()
   }
-  document.getElementById("ExM").innerHTML=car.company + ", "+car.model+", "+state.bad;
+  catch{
+    document.getElementById("ExP").innerHTML="The function was not found. this worked."
+  }
 }
